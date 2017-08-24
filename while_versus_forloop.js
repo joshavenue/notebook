ff43e2908b741bfc6288a918a7dfd1c3b86a8de1
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-24 20:17:42 +0800

    Create while_versus_forloop.js

---
+++ while_versus_forloop.js
@@ -0,0 +1,11 @@
+var counter = 0;
+while(counter < 10){
+  document.write('hi');
+  counter += 1;
+}
+//a while loop
+//--------------------------------------------//
+for (var counter = 0; counter < 10; counter += 1){
+  document.write('hi');
+}
+//a for loop
