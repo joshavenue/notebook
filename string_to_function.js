fbb0e90c84a68a21d2d5cdb8be66260ea10e5940
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-25 01:13:21 +0800

    Create string_to_function.js

---
+++ string_to_function.js
@@ -0,0 +1,9 @@
+function takeOrder(topping, crustType){
+  console.log('Order: pizza with ' + topping + ' that comes along with ' + crustType);
+}
+
+takeOrder('Bacon', 'Cheesy crust');
+takeOrder('chicken', 'Nut crust');
+takeOrder('fish', 'dem crust');
+
+//this is a note for me to remember that i can add str into function //
