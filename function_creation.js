d31e61609f3341d6225d55e19119dcb1a256dc60
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-08-25 10:21:18 +0800

    Create function_creation.js

---
+++ function_creation.js
@@ -0,0 +1,13 @@
+var laundryRoom = 'Basement';
+var mailRoom = 'Room 1A';
+
+var myApartment = function() {
+  var mailBoxNumber = 'Box 3';
+  var laundryRoom = 'In-unit';
+  console.log('Mail box: ' + mailBoxNumber + ', Laundry:' + laundryRoom);
+};
+
+myApartment();
+
+// You can also write function in a way that a variable is equal to a function and declare the function //
+// within the same line//
